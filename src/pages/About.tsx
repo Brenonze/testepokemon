17bb4c64edbdef56a2c829f1118fa6cf405fb473
Breno Franco <130513231+Brenonze@users.noreply.github.com>
2024-12-03 22:44:27 -0300

    Update About.tsx

---
+++ src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About: React.FC = () => (
+  <div>
+    <h1>About</h1>
+    <p>This is a simple React app built with TypeScript.</p>
+  </div>
+);
+
+export default About;
